3dbcc3c2fdfd3d922c98a461354ac9e5777a47d1
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-04-08 21:31:23 -0600

    made it better

--- src/main/kotlin/me/austin/rush/bus/impl/EventBusImpl.kt
+++ src/main/kotlin/me/austin/rush/bus/impl/EventBusImpl.kt
@@ -1,32 +1,65 @@
 package me.austin.rush.bus.impl
 
-import me.austin.rush.annotation.EventHandler
+import me.austin.rush.bus.EventBus
 import me.austin.rush.listener.Listener
-import me.austin.rush.listener.impl.LambdaListener
-import java.lang.reflect.Field
-import java.util.*
-import java.util.Arrays.*
+import me.austin.rush.annotation.EventHandler
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.CopyOnWriteArraySet
-import java.util.stream.Collectors
+import java.util.Arrays
+import java.util.stream.Stream
+import java.lang.reflect.Field
+import kotlin.reflect.KClass
 
-open class EventManager(type: Class<out Listener<*>> = LambdaListener::class.java) : AbstractEventBus(type) {
-    fun registerFields(subscriber: Any) {
-        return when (subscriber) {
-            is Listener<*> -> listOf(subscriber)
-            is Collection<*> -> subscriber.stream().filter { this.type.isAssignableFrom(it!!::class.java) }.collect(Collectors.toList()) as List<Listener<*>>
-            else -> subscriber.javaClass.declaredFields.filter(this::isValid) as List<Listener<*>>
-        }.stream().forEach({ listener ->
-            this.registry.getOrPut(listener.target, ::CopyOnWriteArraySet).run {
-                this.add(listener)
-                this.toSortedSet(Comparator.comparingInt(Listener<*>::priority))
-            }
-        })
+open class EventManager(private val type: Class<out Listener<*>>) : EventBus {
+    override val registry: MutableMap<KClass<*>, MutableSet<Listener<*>>> = ConcurrentHashMap()
+
+    private val subscribers: MutableSet<Any> = CopyOnWriteArraySet()
+
+    override fun register(listener: Listener<*>) {
+        this.registry.getOrPut(listener.target, ::CopyOnWriteArraySet).let {
+            it.add(listener)
+            it.toSortedSet()
+        }
     }
 
-    fun unregisterFields(subscriber: Any) {
-        stream(subscriber.javaClass.declaredFields).filter(this::isValid).forEach { field ->
-            this.registry[field.type.kotlin]?.remove(field.get(subscriber))
+    override fun unregister(listener: Listener<*>) {
+        this.registry[listener.target]?.remove(listener)
+    }
+
+    override fun register(subscriber: Any) {
+        if (isRegistered(subscriber)) return
+
+        this.filter(subscriber::class.java.fields).forEach(this::register)
+
+        this.subscribers.add(subscriber)
+    }
+
+    override fun unregister(subscriber: Any) {
+        if (!isRegistered(subscriber)) return
+
+        this.filter(subscriber::class.java.fields).forEach(this::unregister)
+
+        this.subscribers.remove(subscriber)
+    }
+
+    override fun isRegistered(subscriber: Any): Boolean = this.subscribers.contains(subscriber)
+
+    override fun <T> dispatch(event: T): T {
+        if (this.registry[event!!::class]?.size != 0) {
+            this.getList(event.javaClass).stream().forEach { listener ->
+                listener(event)
+            }
         }
+
+        return event
+    }
+
+    private fun filter(list: Array<out Field>): Stream<Listener<*>> {
+        return Arrays.stream(list).filter(this::isValid) as Stream<Listener<*>>
+    }
+
+    private fun <T : Any> getList(clazz: Class<T>): CopyOnWriteArraySet<Listener<T>> {
+        return this.registry[clazz.kotlin] as CopyOnWriteArraySet<Listener<T>>
     }
 
     private fun isValid(field: Field): Boolean {
