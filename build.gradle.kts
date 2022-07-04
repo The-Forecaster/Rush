b21765eca29dcdfce2231ffb09b3c13f3cfc0685
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-07-04 02:36:26 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     java
     kotlin("jvm") version "1.7.0"
-    id("org.jetbrains.dokka") version "1.7.0"
 }
 
-version "2.1"
+version "2.1.1"
 group "me.austin.rush"
 
 repositories {
     mavenCentral()
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 dependencies {
@@ -18,10 +22,6 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2") {
         exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
-        exclude("org.jetbrains.kotlin", "kotlin-stdlib-common")
-    }
-
-    implementation("org.jetbrains.kotlin:kotlin-test") {
         exclude("org.jetbrains.kotlin", "kotlin-stdlib-common")
     }
     
@@ -36,23 +36,12 @@ dependencies {
     compileOnly("org.jetbrains:annotations:23.0.0")
 }
 
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
 tasks {
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
     }
 
     withType<JavaCompile> {
-        targetCompatibility = "17"
-        sourceCompatibility = "17"
         options.encoding = "UTF-8"
-    }
-
-    named<Jar>("javadocJar") {
-        from(named("dokkaJavadoc"))
     }
 }
