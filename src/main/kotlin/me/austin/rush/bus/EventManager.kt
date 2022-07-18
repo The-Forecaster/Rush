6a3012e1b71f7767eb63c75bbd64d6712c3877a1
The-Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-07-18 03:06:15 -0600

    Update EventManager.kt

--- src/main/kotlin/me/austin/rush/bus/EventManager.kt
+++ src/main/kotlin/me/austin/rush/bus/EventManager.kt
@@ -35,7 +35,7 @@ open class EventManager : EventBus {
         return@let true
     }
 
-    override fun unregister(listener: Listener<*>) = this.registry[listener.target]?.remove(listener) ?: false
+    override fun unregister(listener: Listener<*>) = this.registry[listener.target]?.remove(listener) ?: throw RuntimeException("This listener was never registered")
 
     override fun register(subscriber: Any): Boolean = this.cache.getOrPut(subscriber, subscriber::listeners).map(::register).all()
 
