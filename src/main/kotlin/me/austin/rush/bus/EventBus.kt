5931da19b911a4411ddcfaa711d467b9123481e2
Austin the Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-09-25 14:31:50 -0600

    Update EventBus.kt

--- src/main/kotlin/me/austin/rush/bus/EventBus.kt
+++ src/main/kotlin/me/austin/rush/bus/EventBus.kt
@@ -21,7 +21,6 @@ interface EventBus {
      * Adds the listener into the registry
      *
      * @param listener instance of listener<T> to subscribe
-     * @return true if the listener was added, false if it was already present
      */
     fun register(listener: Listener<*>)
 
@@ -29,7 +28,6 @@ interface EventBus {
      * Adds all listeners to the registry
      *
      * @param listeners all listeners you want to be added
-     * @return true if all listeners were added, false if any weren't
      */
     fun registerAll(vararg listeners: Listener<*>) {
         for (listener in listeners) this.register(listener)
@@ -39,7 +37,6 @@ interface EventBus {
      * Adds all listeners in an iterable to the registry
      *
      * @param listeners the iterable of listeners you want to be added
-     * @return true if all listeners were added, false if any weren't
      */
     fun registerAll(listeners: Iterable<Listener<*>>) {
         for (listener in listeners) this.register(listener)
@@ -49,7 +46,6 @@ interface EventBus {
      * Removes the listener from the registry
      *
      * @param listener listener object to be removed
-     * @return true if it was removed, false if it couldn't be removed or target isn't present in the registry
      */
     fun unregister(listener: Listener<*>)
 
@@ -58,7 +54,6 @@ interface EventBus {
      *
      * @param listeners listener objects you want to be removed
      * @see unregister
-     * @return true if all were removed, false if any could not be removed
      */
     fun unregisterAll(vararg listeners: Listener<*>) {
         for (listener in listeners) this.unregister(listener)
@@ -69,7 +64,6 @@ interface EventBus {
      *
      * @param listeners iterable of listeners you want to be removed
      * @see unregister
-     * @return true if all were removed, false if any could not be
      */
     fun unregisterAll(listeners: Iterable<Listener<*>>) {
         for (listener in listeners) this.unregister(listener)
@@ -79,7 +73,6 @@ interface EventBus {
      * Adds all annotated listeners into the registry
      *
      * @param subscriber object you want to be searched for listeners to be added to the registry
-     * @return true if all listeners inside the class could be registered, false if any could not
      */
     fun register(subscriber: Any)
 
@@ -87,7 +80,6 @@ interface EventBus {
      * Adds all objects and their contained listeners to the registry
      *
      * @param subscribers all subscribers you want to be added to the registry
-     * @return true if all listeners inside all the objects could be registered, false if any could not
      */
     fun registerAll(vararg subscribers: Any) {
         for (subscriber in subscribers) this.register(subscriber)
@@ -97,7 +89,6 @@ interface EventBus {
      * Removes all annotated listeners from the registry
      *
      * @param subscriber event subscriber instance
-     * @return true if all listeners inside this object could be removed from the registry, false if any could not
      */
     fun unregister(subscriber: Any)
 
@@ -105,7 +96,6 @@ interface EventBus {
      * Removes all objects and their contained listeners to the registry
      *
      * @param subscribers all subscribers you want removed from the registry
-     * @return true if all listeners inside all the objects could be registered, false if any could not
      */
     fun unregisterAll(vararg subscribers: Any) {
         for (subscriber in subscribers) this.unregister(subscriber)
