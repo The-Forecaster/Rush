975fba19819bc566379d38a9274c767d27d0c365
Austin the Forecaster <56243908+The-Forecaster@users.noreply.github.com>
2022-10-23 21:59:03 -0600

    fixed files and made it all smaller

--- src/main/kotlin/me/austin/rush/API.kt
+++ src/main/kotlin/me/austin/rush/API.kt
@@ -110,3 +110,24 @@ interface EventBus {
      */
     fun <T : Any> dispatch(event: T)
 }
+
+/**
+ * Basic structure for an event listener and invoker.
+ *
+ * @author Austin
+ */
+interface Listener<T : Any> {
+
+    /** the class of the target event */
+    val target: KClass<T>
+
+    /** the priority that the listener will be called upon(use wisely) */
+    val priority: Int
+
+    /**
+     * Processes an event passed through this listener
+     *
+     * @param param event object that is being processed
+     */
+    operator fun invoke(param: T)
+}
--- src/main/kotlin/me/austin/rush/Impl.kt
+++ src/main/kotlin/me/austin/rush/Impl.kt
@@ -2,8 +2,89 @@ package me.austin.rush
 
 import kotlinx.coroutines.runBlocking
 import net.jodah.typetools.TypeResolver
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArrayList
 import java.util.function.Consumer
+import kotlin.reflect.KCallable
 import kotlin.reflect.KClass
+import kotlin.reflect.full.declaredMembers
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.isSubtypeOf
+import kotlin.reflect.jvm.isAccessible
+import kotlin.reflect.typeOf
+
+/**
+ * Basic implementation of [EventBus]
+ */
+open class EventManager : EventBus {
+    override val registry = ConcurrentHashMap<KClass<*>, MutableList<Listener<*>>>()
+
+    private val cache = ConcurrentHashMap<Any, MutableList<Listener<*>>>()
+
+    override fun register(listener: Listener<*>) {
+        this.registry.getOrPut(listener.target, ::CopyOnWriteArrayList).let {
+            if (it.contains(listener)) return
+
+            var index = 0
+
+            while (index < it.size) {
+                if (it[index].priority < listener.priority) break
+
+                index++
+            }
+
+            it.add(index, listener)
+        }
+    }
+
+    override fun unregister(listener: Listener<*>) {
+        this.registry[listener.target]?.remove(listener)
+    }
+
+    override fun register(subscriber: Any) {
+        for (listener in this.cache.getOrPut(subscriber, subscriber::listeners)) this.register(listener)
+    }
+
+    override fun unregister(subscriber: Any) {
+        for (listener in subscriber.listeners) this.unregister(listener)
+    }
+
+    override fun <T : Any> dispatch(event: T) {
+        (registry[event::class] as? MutableList<Listener<T>>)?.let { synchronized(it) { it.forEach { it(event) } } }
+    }
+
+    fun <T : Cancellable> dispatch(event: T): T {
+        (registry[event::class] as? MutableList<Listener<T>>)?.let {
+            synchronized(it) {
+                for (listener in it) {
+                    listener(event)
+                    if (event.isCancelled) break
+                }
+            }
+        }
+
+        return event
+    }
+}
+
+// Most of this is pasted from bush https://github.com/therealbush/eventbus-kotlin, check him out if you want to see actually good code
+
+private val KCallable<*>.isListener
+    get() = this.findAnnotation<EventHandler>() != null && this.returnType.isSubtypeOf(typeOf<Listener<*>>())
+
+private val <T : Any> KClass<T>.listeners
+    get() = this.declaredMembers.filter(KCallable<*>::isListener) as List<KCallable<Listener<*>>>
+
+private val Any.listeners
+    get() = this::class.listeners.mapTo(mutableListOf()) { it.handleCall(this) }
+
+private fun <T : Any> KCallable<T>.handleCall(receiver: Any? = null): T {
+    val accessible = this.isAccessible
+    this.isAccessible = true
+
+    // This will get a both static and non-static listeners in the jvm
+    return try { call(receiver) } catch (e: Throwable) { call() } finally { this.isAccessible = accessible }
+}
 
 /**
  * This is for making simple, non-verbose listeners
@@ -85,24 +166,3 @@ private val <T : Any> Consumer<T>.paramType
  * Annotate a listener with this class to mark it for adding to the eventbus registry
  */
 annotation class EventHandler
-
-/**
- * Basic structure for an event listener and invoker.
- *
- * @author Austin
- */
-interface Listener<T : Any> {
-
-    /** the class of the target event */
-    val target: KClass<T>
-
-    /** the priority that the listener will be called upon(use wisely) */
-    val priority: Int
-
-    /**
-     * Processes an event passed through this listener
-     *
-     * @param param event object that is being processed
-     */
-    operator fun invoke(param: T)
-}
